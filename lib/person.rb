213d849692fb9500fef65f1eee851bd696d26a23
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-09 19:41:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(arguments)
-    arguments.each do |key, value|
+  def initialize(atr)
+    atr.each do |key, value|
       self.send("#{key}=", value)
     end
   end
